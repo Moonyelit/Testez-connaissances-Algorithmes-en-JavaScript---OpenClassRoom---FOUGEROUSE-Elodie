1e7deedd1d3da9607a4a9bfb98dc2b5673f4c2a9
Moonyelit <fougerouse.elodie@gmail.com>
2026-05-03 16:36:00 +0200

    ✅ Exercice 2

--- Exercice2/index.js
+++ Exercice2/index.js
@@ -2,15 +2,19 @@ function calculate(a, b, operator) {
   const numA = Number(a);
   const numB = Number(b);
 
-  switch (operator) {
-    case '+': return numA + numB;
-    case '-': return numA - numB;
-    case '*': return numA * numB;
-    case '/':
-      if (numB === 0) return 'Division by zero is not allowed';
-      return numA / numB;
-    default:
-      return 'Invalid operator';
+  if (operator === '+') {
+    return numA + numB;
+  } else if (operator === '-') {
+    return numA - numB;
+  } else if (operator === '*') {
+    return numA * numB;
+  } else if (operator === '/') {
+    if (numB === 0) {
+      return 'Division by zero is not allowed';
+    }
+    return numA / numB;
+  } else {
+    return 'Invalid operator';
   }
 }
 
